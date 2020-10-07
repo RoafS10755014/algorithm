cd54b679e3def01f6cfe690411d0518ec31c6ff3
RoafS10755014 <71767960+RoafS10755014@users.noreply.github.com>
2020-10-07 20:02:05 +0800

    Add files via upload

---
+++ ts_analysis.py
@@ -0,0 +1,26 @@
+#判斷某個月是否為31天
+
+year = {1:31, 2:28, 3:31, 4:30, 5:31,
+        6:30, 7:31, 8:31, 9:30, 10:31,
+        11:30, 12:31}
+
+#time complecity
+def timedetect(month):
+    if(year[month])==31:
+        return True
+    else:
+        return False
+
+#space complecity
+def spacedetect(month):
+    days31 = [1, 3, 5, 7, 8, 10, 12]
+    if(month in days31):
+        return True
+    else:
+        return False
+
+print("detect every month if it has 31 days")
+for i in range(12):
+    i+=1;
+    print("time:", i, "is ", end="");print(timedetect(i))
+    print("spece", i, "is ", end="");print(spacedetect(i))
